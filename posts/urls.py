47bed2106c99cf017442836c1fae44dab127945e
kelvinokumba <okumbakelvin@gmail.com>
2023-11-15 18:01:18 +0300

    django we go

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='posts_home'),   
+    path('greet/',views.greet,name="greet_name"),
+    path('posts/',views.return_all_posts,name="all_posts"),
+    #here we have created a path for onr id request
+    #we must specify the data type being an int followed by the id of the dict
+    path('posts/<int:post_id>/',views.return_one_post, name="one_post")
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,8 +2,44 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+posts=[
+    {
+        "id":1,
+        "title":"Learn django",
+        "author":"Ssali"
+    },
+    {  
+        "id":2,
+        "title":"Learn Flask",
+        "author":"Ssali"
+    }, 
+    
+    {
+        "id":3,
+        "title":"Learn Javascrpit",
+        "author":"code mosh"
+    }
+  
+]
+
+
+
 def index(request:HttpRequest):
     return HttpResponse("Hello Django")
 
 
+def greet(request:HttpRequest):
+    name= request.GET.get("name") or "world"
+    
+    return HttpResponse(f"Hello {name}")
 
+def return_all_posts(request:HttpRequest):
+    return HttpResponse(str(posts))
+
+def return_one_post(request:HttpRequest, post_id):
+    for post in posts:
+        if post["id"]==post_id:
+        
+            return HttpResponse(str(post))
+        
+    return HttpRequest("Not Found")
